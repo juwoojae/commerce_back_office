285c3eeed34ea6200c2e82585a1b232a38f57788
Juwoojae <juwqq1234@naver.com>
2025-10-31 19:25:26 +0900

    Initial commit

---
+++ src/main/java/com/example/commerce_back_office/CommerceBackOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.example.commerce_back_office;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceBackOfficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceBackOfficeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/commerce_back_office/CommerceBackOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commerce_back_office;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceBackOfficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
