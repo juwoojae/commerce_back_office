7415016777e6428fa7930fb15da517a5b05dbb9f
Juwoojae <juwqq1234@naver.com>
2025-10-31 20:52:30 +0900

    develop init 2

--- src/main/java/com/example/commerce_back_office/CommerceBackOfficeApplication.java
+++ src/main/java/com/example/commerce_back_office/CommerceBackOfficeApplication.java
@@ -2,7 +2,7 @@ package com.example.commerce_back_office;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//develop 을 위한
 @SpringBootApplication
 public class CommerceBackOfficeApplication {
 
