1d36f18154c9c01bafa8730d46af0eb3ed5452f7
Juwoojae <juwqq1234@naver.com>
2025-11-04 15:14:50 +0900

    feat: 커스텀 예외 추가 구현(#3) - ExpiredException 구현

---
+++ src/main/java/com/example/commerce_back_office/exception/ExpiredException.java
@@ -0,0 +1,7 @@
+package com.example.commerce_back_office.exception;
+
+public class ExpiredException extends RuntimeException {
+    public ExpiredException(String message) {
+        super(message);
+    }
+}
