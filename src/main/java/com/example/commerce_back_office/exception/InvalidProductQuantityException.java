870d9304cce14339472927464bfb0fbb8f51cce7
Juwoojae <juwqq1234@naver.com>
2025-11-06 00:34:58 +0900

    feat: Product ExceptionHandler 구현 (#7) - 커스텀 예외 추가 - 글로벌 예외 처리 추가

--- src/main/java/com/example/commerce_back_office/exception/InvalidProductQuantityException.java
+++ src/main/java/com/example/commerce_back_office/exception/InvalidProductQuantityException.java
@@ -4,14 +4,14 @@ import com.example.commerce_back_office.exception.code.ErrorCode;
 import lombok.Getter;
 
 /**
- * 토큰이 시그니처 검증에 실패했을때 던지는 예외
+ * Product 의 수량을 0 이하로 수정하려고 할때 던져지는 예외
  */
 @Getter
-public class InvalidTokenException extends RuntimeException {
+public class InvalidProductQuantityException extends RuntimeException {
 
     private final ErrorCode errorCode;
 
-    public InvalidTokenException(ErrorCode errorCode) {
+    public InvalidProductQuantityException(ErrorCode errorCode) {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
     }
--- src/main/java/com/example/commerce_back_office/exception/code/ErrorCode.java
+++ src/main/java/com/example/commerce_back_office/exception/code/ErrorCode.java
@@ -11,8 +11,17 @@ public enum ErrorCode implements BaseCode {
     TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
     JSON_PARSING_FAILED(HttpStatus.BAD_REQUEST, "요청 데이터를 파싱할 수 없습니다."),
 
-    //커스텀 예외
-    EMAIL_ALREADY_EXIST(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다.");
+
+    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 상품이 없습니다."),
+    ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 주문이 없습니다."),
+    REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 리뷰가 없습니다."),
+
+    //도메인 예외
+    EMAIL_ALREADY_EXIST(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다."),
+    INVALIDATE_QUANTITY(HttpStatus.BAD_REQUEST, "재고 수량은 0 이하로 설정할 수 없습니다."),
+
+    //@Validation 유효성 검사 실패시 처리
+    VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "입력값이 유효하지 않습니다.");
 
     private final HttpStatus status;
     private final String message;
